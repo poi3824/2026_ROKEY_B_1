6b9c211de68dec8ea0bfe0922788c886d16cc354
poi3824 <poi3824@gmail.com>
2026-07-27 03:53:27 -0400

    pixel

--- src/error_fix/error_fix/error_fix.py
+++ src/error_fix/error_fix/error_fix.py
@@ -91,7 +91,7 @@ class BatteryAssemblyVisionNode(Node):
         # 제어 및 오차 보정 파라미터
         # ----------------------------------------------------------------------
         self.TOLERANCE_DEG = 0.5        # 목표 각도 정밀도: 0.5도 이하
-        self.MAX_VALID_PIXEL_ERR = 400  # 이상치 픽셀 오차 스킵 가드 (배터리 중점 스캔 오차로 초기 오프셋이 커도 추적 유지되도록 200->400)
+        self.MAX_VALID_PIXEL_ERR = 200  # 이상치 픽셀 오차 스킵 가드 (배터리 중점 스캔 오차로 초기 오프셋이 커도 추적 유지되도록 200->400)
 
         # 최신 Depth 프레임 및 상태 변수
         self.current_depth_frame = None
